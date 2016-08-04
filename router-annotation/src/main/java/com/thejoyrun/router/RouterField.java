209777d6bc2fd66b30ba26fc99a9c68bef58ac12
Wiki <taoweiji2008@qq.com>
2016-08-04 16:45:14 +0800

    2

--- router-annotation/src/main/java/com/thejoyrun/router/RouterField.java
+++ router-annotation/src/main/java/com/thejoyrun/router/RouterField.java
@@ -13,5 +13,5 @@ import java.lang.annotation.Target;
 @Target(ElementType.FIELD)
 @Retention(RetentionPolicy.CLASS)
 public @interface RouterField {
-    String value();
+    String[] value();
 }
--- router/src/main/java/com/thejoyrun/router/Routers.java
+++ router/src/main/java/com/thejoyrun/router/Routers.java
@@ -49,36 +49,38 @@ public class Routers {
                 continue;
             }
             String type = field.getGenericType().toString();
-            String name = annotation.value();
             field.setAccessible(true);
+            String[] names = annotation.value();
             try {
-                if (!bundle.containsKey(name)) {
-                    continue;
-                }
-                if (type.equals("double")) {
-                    field.set(activity, bundle.getDouble(name, field.getDouble(activity)));
-                    continue;
-                } else if (type.equals("float")) {
-                    field.set(activity, bundle.getFloat(name, field.getFloat(activity)));
-                    continue;
-                } else if (type.equals("int")) {
-                    field.set(activity, bundle.getInt(name, field.getInt(activity)));
-                    continue;
-                } else if (type.equals("boolean")) {
-                    field.set(activity, bundle.getBoolean(name, field.getBoolean(activity)));
-                    continue;
-                }
-                Object defaultValue = field.get(activity);
-                if (field.getGenericType() == String.class) {
-                    field.set(activity, bundle.getString(name, (String) defaultValue));
-                } else if (field.getGenericType() == Double.class) {
-                    field.set(activity, bundle.getDouble(name, defaultValue != null ? (Double) defaultValue : 0));
-                } else if (field.getGenericType() == Float.class) {
-                    field.set(activity, bundle.getFloat(name, defaultValue != null ? (Float) defaultValue : 0));
-                } else if (field.getGenericType() == Integer.class) {
-                    field.set(activity, bundle.getInt(name, defaultValue != null ? (Integer) defaultValue : 0));
-                } else if (field.getGenericType() == Boolean.class) {
-                    field.set(activity, bundle.getBoolean(name, defaultValue != null ? (Boolean) defaultValue : false));
+                for (String name : names){
+                    if (!bundle.containsKey(name)) {
+                        continue;
+                    }
+                    if (type.equals("double")) {
+                        field.set(activity, bundle.getDouble(name, field.getDouble(activity)));
+                        continue;
+                    } else if (type.equals("float")) {
+                        field.set(activity, bundle.getFloat(name, field.getFloat(activity)));
+                        continue;
+                    } else if (type.equals("int")) {
+                        field.set(activity, bundle.getInt(name, field.getInt(activity)));
+                        continue;
+                    } else if (type.equals("boolean")) {
+                        field.set(activity, bundle.getBoolean(name, field.getBoolean(activity)));
+                        continue;
+                    }
+                    Object defaultValue = field.get(activity);
+                    if (field.getGenericType() == String.class) {
+                        field.set(activity, bundle.getString(name, (String) defaultValue));
+                    } else if (field.getGenericType() == Double.class) {
+                        field.set(activity, bundle.getDouble(name, defaultValue != null ? (Double) defaultValue : 0));
+                    } else if (field.getGenericType() == Float.class) {
+                        field.set(activity, bundle.getFloat(name, defaultValue != null ? (Float) defaultValue : 0));
+                    } else if (field.getGenericType() == Integer.class) {
+                        field.set(activity, bundle.getInt(name, defaultValue != null ? (Integer) defaultValue : 0));
+                    } else if (field.getGenericType() == Boolean.class) {
+                        field.set(activity, bundle.getBoolean(name, defaultValue != null ? (Boolean) defaultValue : false));
+                    }
                 }
             } catch (IllegalAccessException e) {
                 e.printStackTrace();
